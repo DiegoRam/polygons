6d5c00d2ffc5b5525fccf6d70ba5a3c0366fd325
diegoram <ddarkheart@gmail.com>
2014-12-07 13:13:05 -0300

    fix jade inheritance

--- polygons/polygons/settings.py
+++ polygons/polygons/settings.py
@@ -33,6 +33,7 @@ TEMPLATE_LOADERS = (
     ('pyjade.ext.django.Loader',(
         'django.template.loaders.filesystem.Loader',
         'django.template.loaders.app_directories.Loader',
+        'django.template.loaders.eggs.Loader',
     )),
 )
 
