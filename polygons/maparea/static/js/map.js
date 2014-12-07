b4b8512c5daeeb158680fe0cbe962191a57585cb
diegoram <ddarkheart@gmail.com>
2014-12-07 18:06:55 -0300

    fix final slash on static url calls and add custom icon for markers

--- polygons/maparea/static/js/map.js
+++ polygons/maparea/static/js/map.js
@@ -8,7 +8,22 @@ $(document).ready(function() {
         };
         map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
+
+        google.maps.event.addListener(map, 'click', function(event){
+            createMarker(event.latLng);
+        });
+    }
+
+    function createMarker(location){
+        console.info('creating new markers at ' + location);
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: '/static/img/pin.png'
+        });
     }
 
     google.maps.event.addDomListener(window, 'load', initialize);
+
+
 });
