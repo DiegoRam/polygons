fb6715a848ddf42c12f70394afc6b873acaa7017
diegoram <ddarkheart@gmail.com>
2014-12-07 14:28:14 -0300

    get templates and js call neater

--- polygons/maparea/static/js/map.js
+++ polygons/maparea/static/js/map.js
@@ -1,13 +1,14 @@
+$(document).ready(function() {
+    var map;
 
-var map;
+    function initialize() {
+        var mapOptions = {
+            zoom: 8,
+            center: new google.maps.LatLng(37.708, -122.280)
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+    }
 
-function initialize() {
-    var mapOptions = {
-        zoom: 10,
-        center: new google.maps.LatLng(-34.397, 150.644)
-    };
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+    google.maps.event.addDomListener(window, 'load', initialize);
+});
