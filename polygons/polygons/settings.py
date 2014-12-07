c337f771fde5be6ae9fdbbaa85cd662c89056f4a
diegoram <ddarkheart@gmail.com>
2014-12-07 11:56:58 -0300

    got mysql connector up and runnning

--- polygons/polygons/settings.py
+++ polygons/polygons/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'polygons.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mozio',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
